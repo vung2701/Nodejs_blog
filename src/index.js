6b332638e5d3664633a7b8025566d7dadb0bb7f6
vung2701 <vungnguyen2701@gmail.com>
2023-02-15 16:26:11 +0700

    static files & node sass

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 const morgan = require('morgan');
 const app = express();
-const port = 3001;
+const port = 3000;
+
+// Static url
+app.use(express.static(path.join(__dirname,'public')));
 
 // https logger
 app.use(morgan('combined'));
@@ -13,7 +17,7 @@ app.engine('hbs', handlebars.engine({
 }));
 
 app.set('view engine', 'hbs');
-app.set("views", "./src/resources/views");
+app.set("views", path.join(__dirname, '/resources/views'));
 
 app.get('/', (req, res) => {
    res.render('home');
