d73e15ae5d55906e4cece87cf81e1fef43891017
vung2701 <vungnguyen2701@gmail.com>
2023-02-15 22:38:38 +0700

    Post method

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+// GET/POST from form
+app.use(express.urlencoded({
+  extended: true
+}))
+// GET/POST from js
+app.use(express.json())
+
 // Static url
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -28,12 +35,14 @@ app.get('/news', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
+  console.log(req.query)
    res.render('search');
 })
 
-// app.post('/search', (req, res) => {
-//    res.render('search');
-// })
+app.post('/search', (req, res) => {
+  console.log(req.body)
+   res.send();
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
