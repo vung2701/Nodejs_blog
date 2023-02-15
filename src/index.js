5111a8701e0f295a623e4bd2587c41a902123cb0
vung2701 <vungnguyen2701@gmail.com>
2023-02-15 22:11:42 +0700

    defauft form behavior

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ app.use(morgan('combined'));
 app.engine('hbs', handlebars.engine({
   extname: '.hbs',
 }));
-
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, '/resources/views'));
+
 
 app.get('/', (req, res) => {
    res.render('home');
@@ -26,6 +26,14 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
    res.render('news');
 })
+
+app.get('/search', (req, res) => {
+   res.render('search');
+})
+
+// app.post('/search', (req, res) => {
+//    res.render('search');
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
